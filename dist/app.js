750f1013fdec408f89664f2cc444d394ce06b7b2
margoar <marcelapaz.ga@gmail.com>
2021-05-15 18:00:50 -0400

    instalacion dependencias

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nombre = void 0;
+exports.nombre = 'Marcela';
+console.log(exports.nombre);
+//# sourceMappingURL=app.js.map
